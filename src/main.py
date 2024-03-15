3ee5cf783d039b01c624564ae0b5661b857aa5d1
Den <serge23023@gmial.com>
2024-03-15 21:34:41 +0300

    edit main

--- src/main.py
+++ src/main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from src.utils import operations_create
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def print_operation():
+    i = 0
+    for operation in operations_create():
+        if i < 5 and operation.state == "EXECUTED":
+            print(f"{operation.date.strftime("%d.%m.%Y")} {operation.description}\n"
+                  f"{operation.from_} -> {operation.to}\n"
+                  f"{operation.operation_amount.amount} {operation.operation_amount.currency.name}\n")
+            i += 1
+        elif i == 5:
+            break
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    print_operation()
