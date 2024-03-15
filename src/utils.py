e258a10dab9017d4711b9efd8d67f25b95420a15
Den <serge23023@gmial.com>
2024-03-15 21:58:23 +0300

    add docstring utils

--- src/utils.py
+++ src/utils.py
@@ -3,12 +3,20 @@ import json
 from src.operation import Operation
 
 
-def open_json():
+def open_json() -> list:
+    """
+    Открывает и загружает JSON-файл.
+    """
     with open('../data/operations.json', 'r', encoding='utf-8') as f:
         return json.load(f)
 
 
-def operations_create():
+def operations_create() -> list[Operation]:
+    """
+    Функция operations_create создает список операций, каждая из которых представляет собой объект Operation,
+    и возвращает отсортированный список этих операций. Если элемент пуст, он пропускается.
+    Ключи в словаре item изменяются перед созданием объекта Operation.
+    """
     operations = []
     for item in open_json():
         if not item:
