887f216c14b6222c429ebb6bae53f3d3a0ea3f30
Den <serge23023@gmial.com>
2024-03-15 22:07:03 +0300

    add docstring currency

--- src/currency.py
+++ src/currency.py
@@ -1,4 +1,10 @@
 class Currency:
+    """
+    Класс Currency представляет валюту.
+    name - название валюты.
+    code - код валюты.
+    """
+
     def __init__(self, name: str, code: str):
         self.name = name
         self.code = code
