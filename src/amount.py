a75b166fab17696e0578e5ea1c138de709cb1ba6
Den <serge23023@gmial.com>
2024-03-15 22:05:30 +0300

    add docstring amount

--- src/amount.py
+++ src/amount.py
@@ -2,6 +2,11 @@ from src.currency import Currency
 
 
 class Amount:
+    """
+    Класс Amount представляет сумму в определенной валюте.
+    Таким образом, каждый объект Amount содержит сумму и объект Currency, представляющий валюту этой суммы.
+    """
+
     def __init__(self, amount: str, currency: dict):
         self.amount = amount
         self.currency = Currency(**currency)
