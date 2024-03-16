8dc168b7ec7952c0605d18645ff1632c606f824a
Den <serge23023@gmial.com>
2024-03-16 17:37:51 +0300

    edit utils

--- src/utils.py
+++ src/utils.py
@@ -1,14 +1,26 @@
 import json
+import os
 
 from src.operation import Operation
 
 
-def open_json() -> list:
+def open_json(file_name: str) -> list:
     """
     Открывает и загружает JSON-файл.
     """
-    with open('../data/operations.json', 'r', encoding='utf-8') as f:
+    with open(find_file(file_name), 'r', encoding='utf-8') as f:
         return json.load(f)
+
+
+def find_file(filename):
+    """
+    Поиск файла по наименованию
+    :return: Путь до файла
+    """
+    for root, dirs, files in os.walk(os.path.dirname(os.getcwd())):
+        if filename in files:
+            return os.path.join(root, filename)
+    return None
 
 
 def operations_create() -> list[Operation]:
@@ -18,7 +30,7 @@ def operations_create() -> list[Operation]:
     Ключи в словаре item изменяются перед созданием объекта Operation.
     """
     operations = []
-    for item in open_json():
+    for item in open_json('operations.json'):
         if not item:
             continue
         item['operation_id'] = item.pop('id')
@@ -27,3 +39,13 @@ def operations_create() -> list[Operation]:
         operations.append(Operation(**item))
     operations.sort(key=lambda operation: operation.date, reverse=True)
     return operations
+
+
+def print_operation(count: int):
+    list_operation = []
+    for operation in operations_create():
+        if len(list_operation) < count and operation.state == "EXECUTED":
+            list_operation.append(operation)
+        elif len(list_operation) == count:
+            break
+    return list_operation
