541c1577d21b58a949d4456219f2ddba687f74e4
Den <serge23023@gmial.com>
2024-03-16 17:36:34 +0300

    edit test_utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,19 +1,28 @@
 import pytest
 from src.operation import Operation
-from src.utils import open_json, operations_create
+from src.utils import open_json, operations_create, print_operation
 
 
 def test_open_json():
     # Проверка, что функция open_json возвращает список
-    assert isinstance(open_json(), list)
+    assert type(open_json('operations.json')) is list
+    with pytest.raises(TypeError):
+        open_json('perations.json')
 
 
 def test_operations_create():
     operations = operations_create()
     # Проверка, что функция operations_create возвращает список
-    assert isinstance(operations, list)
+    assert type(operations) is list
     # Проверка, что все элементы в списке являются экземплярами класса Operation
     assert all(isinstance(operation, Operation) for operation in operations)
+
+
+def test_print_operation(i):
+    operations = print_operation(i)
+    assert len(operations) == i
+    for operation in operations:
+        assert operation.state == "EXECUTED"
 
 
 # Запуск тестов с помощью pytest
