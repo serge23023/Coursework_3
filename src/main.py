c7446ecafbe5d0342c17c5ce59e0e1b2e355074a
Den <serge23023@gmial.com>
2024-03-16 17:37:24 +0300

    edit main

--- src/main.py
+++ src/main.py
@@ -1,18 +1,5 @@
-from src.utils import operations_create
-
-
-def print_operation():
-    """
-    Выводит представление первых пяти выполненных операций.
-    """
-    i = 0
-    for operation in operations_create():
-        if i < 5 and operation.state == "EXECUTED":
-            print(operation.__repr__())
-            i += 1
-        elif i == 5:
-            break
-
+from src.utils import print_operation
 
 if __name__ == '__main__':
-    print_operation()
+    for operations in print_operation(5):
+        print(operations.__repr__())
